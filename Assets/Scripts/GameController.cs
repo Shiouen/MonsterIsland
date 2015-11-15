1cf509c23161432a9c31d85b0e8e77bbfb81f5e6
Olivier Van Aken <oliver_va@live.be>
2015-11-15 18:26:30 +0100

    fixed locking/hiding cursor in GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private bool cursorLocked;
+
+	// Use this for initialization
+	void Start () {
+        // lock cursor
+        Cursor.lockState = (this.cursorLocked) ? CursorLockMode.Locked : Cursor.lockState;
+        Cursor.visible = !this.cursorLocked;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
