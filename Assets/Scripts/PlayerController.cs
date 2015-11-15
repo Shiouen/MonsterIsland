88d5ef15c3b2817bbdca7ef4ffd5fd7e2205931d
Olivier Van Aken <oliver_va@live.be>
2015-11-15 18:03:50 +0100

    fixed viewing angle and mouse controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour {
     private float mouseSensitivity;
     [SerializeField]
     private float movementSpeed;
+    [SerializeField]
+    private float verticalViewAngle;
+
+    private float verticalRotation;
 
     CharacterController characterController;
 
@@ -13,29 +17,38 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
         this.characterController = GetComponent<CharacterController>();
 
-        this.mouseSensitivity = 5f;
-        this.movementSpeed = 10f;
+        this.mouseSensitivity = (this.mouseSensitivity == 0.0f) ? 5.0f : this.mouseSensitivity;
+        this.movementSpeed = (this.movementSpeed == 0.0f) ? 10.0f : this.movementSpeed;
+        this.verticalViewAngle = (this.verticalViewAngle == 0.0f) ? 120.0f : this.verticalViewAngle;
+
+        this.verticalRotation = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        // Rotation
+        /* Rotation */
+
         // horizontal view change
-        float rotateY = Input.GetAxis("Mouse X");
-        this.transform.Rotate(0, rotateY, 0);
-        // vertical view change
-        float rotateX = Input.GetAxis("Mouse Y");
-        Camera.main.transform.Rotate(-rotateX, 0, 0);
+        float rotateY = Input.GetAxis("Mouse X") * mouseSensitivity;
+        this.transform.Rotate(0.0f, rotateY, 0.0f);
+
+        // vertical view change - limited to the chosen view angle
+        this.verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -(verticalViewAngle / 2.0f), (verticalViewAngle / 2.0f));
+
+        Camera.main.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0.0f, 0.0f);
 
 
-        // Movement
+        /* Movement */
+
         // z - move front/backwards
         float z = Input.GetAxis("Vertical") * movementSpeed;
-        // x - move left/right
+        // x - move left/right - 80% of it, since this movement seems to be faster than 
+        // the front/backwards movement with equal speed value
         float x = Input.GetAxis("Horizontal") * movementSpeed * 0.80f;
 
         // need to multiply with rotation, otherwise we keep going 1 way (even if we rotate)
-        Vector3 speed = this.transform.rotation * new Vector3(x, 0f, z);
+        Vector3 speed = this.transform.rotation * new Vector3(x, 0.0f, z);
         
         this.characterController.SimpleMove(speed);
 	}
